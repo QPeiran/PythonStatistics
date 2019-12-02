0cfa3a72fc276eee50d2b498e8c51e2dabdbec86
QPeiran <quanpeiran@gmail.com>
2019-12-02 21:43:19 +1300

    ploting normalized

--- sklearn_normalization.py
+++ sklearn_normalization.py
@@ -1,20 +1,30 @@
 
 import pandas as pd
 print("sklearn_normalization.py")
-
-
 from sklearn import preprocessing
 import numpy as np
+import matplotlib.pyplot as plt
 # Get dataset
 df = pd.read_csv("california_housing_train.csv", sep=",")
 # Normalize total_bedrooms column
 x_array = np.array(df['total_bedrooms'])
 normalized_X = preprocessing.normalize([x_array])
-print(df)
+print(x_array)
+print(len(x_array))
+print(normalized_X[0])
+print(len(normalized_X[0]))
+
+M =  len(normalized_X[0])
+# Get column names first
+names = df.columns
+# Create the Scaler object
+scaler = preprocessing.StandardScaler()
+# Fit your data on the scaler object
+scaled_df = scaler.fit_transform(df)
+scaled_df = pd.DataFrame(scaled_df, columns=names)
+
 
 import matplotlib.pyplot as plt
-plt.plot([1, 2, 3, 4])
-plt.ylabel('some numbers')
+plt.plot(normalized_X[0], 'ro')
+#plt.ylabel('some numbers')
 plt.show()
-
-
