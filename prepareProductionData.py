e1dd0d0cfff6fae7a26e5d5aad4ef0b2cd507a90
QPeiran <quanpeiran@gmail.com>
2019-12-16 12:55:17 +1300

    embeding KL number (working)

--- prepareProductionData.py
+++ prepareProductionData.py
@@ -1,3 +1,15 @@
 import pandas as pd
-xslx = pd.ExcelFile(r'C:\Users\Peiran Quan\Desktop\W51.xlsx')
+
+data ={}
+with pd.ExcelFile(r'C:\Users\Peiran Quan\Desktop\W51.xlsx') as xlsx:
+    data['KL1'] = pd.read_excel(xlsx,'Kit line 1',index_col = None,na_values='NA')
+    data['KL2'] = pd.read_excel(xlsx,'Kit line 2',index_col = None,na_values='NA')#, skiprows=1)
+
+for key in data:
+    sLength = len(data[key])
+    key_array = [key for i in range(sLength)]
+    data[key]['kittingline'] = key_array
+    print(data[key])
+    #data[key].to_csv(r'C:\Users\Peiran Quan\Desktop\W51staged.csv')
+
 
