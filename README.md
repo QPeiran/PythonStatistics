3f7fcf9106ebe971e081cc3386548f288d7a2475
QPeiran <quanpeiran@gmail.com>
2019-12-18 13:28:21 +1300

    drop_duplicates

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ My Production Data Process Steps:
 
 Step 1: Read raw data in Excel format, then combine 8 kitting line's data into 1 giant sheet;
 (Notes: the kitting line's name is not included in raw data, it's traced by tab's name. Therefore, I add a new column with the kitting line's name)
+
 Step 2: detect any missing data & remove any duplicate data;
 (Notes: basically need to figure out "if all kitting line have finished all production batch")
 
--- prepareProductionData.py
+++ prepareProductionData.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-#read file into data frame
+# read file into data frame
 data ={}
 with pd.ExcelFile(r'C:\Users\Peiran Quan\Desktop\W51.xlsx') as xlsx:
     data['KL1'] = pd.read_excel(xlsx,'Kit line 1',index_col = None,na_values='NA')
@@ -12,7 +12,7 @@ with pd.ExcelFile(r'C:\Users\Peiran Quan\Desktop\W51.xlsx') as xlsx:
     data['KL7'] = pd.read_excel(xlsx,'Kit line 7',index_col = None,na_values='NA')
     data['KL8'] = pd.read_excel(xlsx,'Kit line 8',index_col = None,na_values='NA')
 
-#Join 8 kitting lines' data to one
+# Join 8 kitting lines' data to one
 df = pd.DataFrame(data=None)   # df is the frame containing all raw data
 for KL in data:
     sLength = len(data[KL])
@@ -21,9 +21,20 @@ for KL in data:
     df = pd.concat([df,data[KL]], sort = False)
     #print(df)
 
-#rename all the column index
+# rename all the column index
 df.columns = ['Barcode', 'Production Batch', 'Recipe and P', 'Timestamp', 'Date', 'Seq Code', 'Week', 'Team Leader', 'Replenisher', 'Pickers', 'Break Reasons', 'Missing Products', 'Kitting Line']
 
+"""
+# find duplicate rows
+dup = df.duplicated() # return a Boolean series with True at the place of each duplicated rows except their first occurrence (default value of keep argument is ‘first’). Then pass this Boolean Series to [] operator of Dataframe to select the rows which are duplicate
+print(dup) 
 
-#to delete duplicates and detect missing data
+dup = df[df.duplicated()]
+print(dup) # show duplicated tuples
+"""
+#  delete duplicates
+df = df.drop_duplicates(subset = None, keep='first')
+#  detect missing data
+
+#print to CSV
 df.to_csv(r'C:\Users\Peiran Quan\Desktop\W51staged.csv',index=False)
