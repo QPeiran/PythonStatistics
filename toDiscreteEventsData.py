1676464dfcc7918663b8e22c356f5387ca3fd25e
QPeiran <quanpeiran@gmail.com>
2019-12-20 16:33:09 +1300

    cool for all 8 lines

--- toDiscreteEventsData.py
+++ toDiscreteEventsData.py
@@ -60,28 +60,35 @@ staged_df = pd.read_csv(r'C:\Users\Peiran Quan\Desktop\W51staged.csv')
 #print(dataFrame['Team Leader'].describe())
 df_final = pd.DataFrame(data=None) #df is the event data frame
 
-df_temp_raw = staged_df.loc[staged_df['Kitting Line'] == 'KL1']
-df_temp_event = pd.DataFrame(columns=('Event_id', 'Start Time', 'Finish Time', 'Activity', 'Recipe Name', 'Break Reasons', 'Missing Ingredients', 'Kitting Line', 'Assembly Batch', 'Event Shift', 'Team Leader', 'Pickers Count', 'Time Consumption'))
-#print(df_temp)
-df_temp_event['Finish Time'] = df_temp_raw['Timestamp and Date']
-df_temp_event['Start Time'][0] = '2019-1-1 00:00:00'
-df_temp_event['Recipe Name'][1:3] = df_temp_raw['Recipe and P'].loc[df_temp_raw['Recipe and P'].first_valid_index()]
-#df_temp_event['Recipe Name'][1] = df_temp_raw['Recipe and P'].loc[df_temp_raw['Recipe and P'].first_valid_index()]
+df_temp_raw = staged_df.loc[staged_df['Kitting Line'] == 'KL4']
 
-for i in range (len(df_temp_raw)):
-    df_temp_event['Start Time'].loc[i+1] = df_temp_raw['Timestamp and Date'].loc[i]
-    df_temp_event['Activity'].loc[i] = activity_dependency(df_temp_raw['Barcode'].loc[i])
-    df_temp_event['Break Reasons'].loc[i+1] = df_temp_raw['Break Reasons'].loc[i]
-    df_temp_event['Missing Ingredients'].loc[i+1] = df_temp_raw['Missing Products'].loc[i]
-    df_temp_event['Kitting Line'].loc[i+1] = df_temp_raw['Kitting Line'].loc[i]
-    df_temp_event['Assembly Batch'].loc[i+1] = batchcode_dependency(df_temp_raw['Production Batch'].loc[i])
-    df_temp_event['Time Consumption'].loc[i] = time_consumption(df_temp_event['Start Time'].loc[i], df_temp_event['Finish Time'].loc[i])
-    df_temp_event['Event Shift'].loc[i] = categorize_event_shift(df_temp_event['Finish Time'].loc[i])
-    df_temp_event['Team Leader'].loc[i] = df_temp_raw['Team Leader'].loc[i]
-    df_temp_event['Pickers Count'].loc[i] = count_pickers(df_temp_raw['Pickers'].loc[i])
 
-for j in range (3,len(df_temp_raw)):
-    reversed_index = df_temp_raw['Recipe and P'][::-1].loc[j:].first_valid_index()
-    df_temp_event['Recipe Name'].loc[j] = df_temp_raw['Recipe and P'].loc[reversed_index]
-
-print(df_temp_event)
+def main(df_temp_raw):
+    df_temp_event = pd.DataFrame(columns=('Start Time', 'Finish Time', 'Activity', 'Recipe Name', 'Break Reasons', 'Missing Ingredients', 'Kitting Line', 'Assembly Batch', 'Event Shift', 'Team Leader', 'Pickers Count', 'Time Consumption'))
+    index_align = df_temp_raw.first_valid_index() # pandas is using the df_temp_raw's frame index whenever df_temp_raw is called
+    df_temp_event['Finish Time'] = df_temp_raw['Timestamp and Date']
+    df_temp_event['Start Time'][index_align] = '2019-1-1 00:00:00'
+    df_temp_event['Recipe Name'][1:3] = df_temp_raw['Recipe and P'].loc[df_temp_raw['Recipe and P'].first_valid_index()]
+    #df_temp_event['Recipe Name'][1] = df_temp_raw['Recipe and P'].loc[df_temp_raw['Recipe and P'].first_valid_index()]
+    #print(df_temp_event)
+    
+    for i in range (len(df_temp_raw)):
+        df_temp_event['Start Time'].loc[index_align+i+1] = df_temp_raw['Timestamp and Date'].loc[index_align+i]
+        df_temp_event['Activity'].loc[index_align+i] = activity_dependency(df_temp_raw['Barcode'].loc[index_align+i])
+        df_temp_event['Break Reasons'].loc[index_align+i+1] = df_temp_raw['Break Reasons'].loc[index_align+i]
+        df_temp_event['Missing Ingredients'].loc[index_align+i+1] = df_temp_raw['Missing Products'].loc[index_align+i]
+        df_temp_event['Kitting Line'].loc[index_align+i+1] = df_temp_raw['Kitting Line'].loc[index_align+i]
+        df_temp_event['Assembly Batch'].loc[index_align+i+1] = batchcode_dependency(df_temp_raw['Production Batch'].loc[index_align+i])
+        df_temp_event['Time Consumption'].loc[index_align+i] = time_consumption(df_temp_event['Start Time'].loc[index_align+i], df_temp_event['Finish Time'].loc[index_align+i])
+        df_temp_event['Event Shift'].loc[index_align+i] = categorize_event_shift(df_temp_event['Finish Time'].loc[index_align+i])
+        df_temp_event['Team Leader'].loc[index_align+i] = df_temp_raw['Team Leader'].loc[index_align+i]
+        df_temp_event['Pickers Count'].loc[index_align+i] = count_pickers(df_temp_raw['Pickers'].loc[index_align+i])
+        #return 0
+       
+    for j in range (3,len(df_temp_raw)):
+        reversed_index = df_temp_raw['Recipe and P'][::-1].loc[(index_align + j):].first_valid_index()
+        df_temp_event['Recipe Name'].loc[reversed_index + j] = df_temp_raw['Recipe and P'].loc[reversed_index]
+    #print(df_temp_raw['Recipe and P'].loc[reversed_index])
+    print(df_temp_event)
+    
+main(df_temp_raw)
