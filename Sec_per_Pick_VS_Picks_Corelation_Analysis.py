0fbfa39fef990b8fade07da3f0e6e12df94ae7b0
QPeiran <quanpeiran@gmail.com>
2020-03-09 13:31:28 +1300

    fitted success : R^2 = 0

--- Sec_per_Pick_VS_Picks_Corelation_Analysis.py
+++ Sec_per_Pick_VS_Picks_Corelation_Analysis.py
@@ -4,20 +4,26 @@ import matplotlib.pyplot as plt
 from scipy import stats
 
 
-
+"""
 np.random.seed()
-x = np.random.random(100)
+x = np.random.random((20,))
+print(x)
+y = 1.6*x + np.random.random(20)
+print(y)
+"""
+dataFrame = pd.read_csv(r'C:\Users\Peiran Quan\Desktop\secperpickVSpicks.csv')
 
-y = 1.6*x + np.random.random(100)
 
+sec_per_pick = dataFrame['Sec per Pick (pure production)']
+picks = dataFrame['Picks']
 
-slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+slope, intercept, r_value, p_value, std_err = stats.linregress(picks, sec_per_pick)
 print("slope: %f    intercept: %f r-value: %f " % (slope, intercept, r_value))
 
 print("R-squared: %f p-value: %f" % (r_value**2, p_value) )
 
-plt.plot(x, y, 'o', label='original data')
-plt.plot(x, intercept + slope*x, 'r', label='fitted line')
+plt.plot(picks, sec_per_pick, 'o', label='original data')
+plt.plot(picks, intercept + slope*picks, 'r', label='fitted line')
 plt.legend()
 plt.show()
 """
