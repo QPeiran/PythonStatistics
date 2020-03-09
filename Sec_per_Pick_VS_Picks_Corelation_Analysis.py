7e5e8aadeed67e3dc02669e59dfe18712d8d2945
QPeiran <quanpeiran@gmail.com>
2020-03-09 14:38:12 +1300

    Box plot group by picks

--- Sec_per_Pick_VS_Picks_Corelation_Analysis.py
+++ Sec_per_Pick_VS_Picks_Corelation_Analysis.py
@@ -6,14 +6,18 @@ from scipy import stats
 
 """
 np.random.seed()
-x = np.random.random((20,))
+x = np.random.random((20,)).
 print(x)
 y = 1.6*x + np.random.random(20)
 print(y)
+
+x = np.array([[0, 1], [0, 2]])
+r = stats.linregress(x)
+r.slope, r.intercept
 """
 dataFrame = pd.read_csv(r'C:\Users\Peiran Quan\Desktop\secperpickVSpicks.csv')
 
-
+##################Line Regression Check
 sec_per_pick = dataFrame['Sec per Pick (pure production)']
 picks = dataFrame['Picks']
 
@@ -26,8 +30,19 @@ plt.plot(picks, sec_per_pick, 'o', label='original data')
 plt.plot(picks, intercept + slope*picks, 'r', label='fitted line')
 plt.legend()
 plt.show()
-"""
-x = np.array([[0, 1], [0, 2]])
-r = stats.linregress(x)
-r.slope, r.intercept
-"""
+
+
+################Box plot group by picks###########
+
+UniquePicks = dataFrame.Picks.unique()
+DataFrameDict = {elem : pd.DataFrame for elem in UniquePicks}
+for key in DataFrameDict.keys():
+    DataFrameDict[key] = dataFrame[:][dataFrame.Picks == key]
+    DataFrameDict[key] = DataFrameDict[key].drop("Week/Recipe", axis = 1)
+#Slicing the origianl data frame
+DF_by_picks = pd.DataFrame(data=None)
+for key in DataFrameDict.keys():
+    DF_by_picks =  pd.concat([DF_by_picks, DataFrameDict[key]], axis = 1, join='outer')
+#print(DF_by_picks)
+
+DF_by_picks.plot.box(grid = 'True')
