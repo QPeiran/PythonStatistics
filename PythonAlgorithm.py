91fd554402228f5be0951414f613dd5eed27ddeb
QPeiran <quanpeiran@gmail.com>
2020-04-30 16:11:26 +1200

    decorators1

--- PythonAlgorithm.py
+++ PythonAlgorithm.py
@@ -2,6 +2,7 @@ import pandas as pd
 import datetime as dt
 import numpy as np
 from math import *
+import time
 #data = pd.read_excel (r'C:\Users\Peiran Quan\Desktop\W51.xlsx') 
 #print(data)
 """
@@ -103,42 +104,97 @@ print(count_pickers(staged_df["Pickers"][100]))
 #     print(False)
 
 # #help(list.__contains__)
-def testA(value, count):
-    if value > 21 and count > 0:
-        value = value - 10
-        count = count - 1
-        return testA(value, count)
-    else:
-        return value
+# def testA(value, count):
+#     if value > 21 and count > 0:
+#         value = value - 10
+#         count = count - 1
+#         return testA(value, count)
+#     else:
+#         return value
 
 
-def blackjack_hand_greater_than(hand_1, hand_2):
+# def blackjack_hand_greater_than(hand_1, hand_2):
 
-    dict_value = {"A":11, "2": 2, "3": 3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":10, "Q":10, "K": 10}
-    hand1_value = 0
-    hand2_value = 0
-    countA_1 = 0
-    countA_2 = 0
+#     dict_value = {"A":11, "2": 2, "3": 3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":10, "Q":10, "K": 10}
+#     hand1_value = 0
+#     hand2_value = 0
+#     countA_1 = 0
+#     countA_2 = 0
 
-    for i in range(len(hand_1)):
-        hand1_value = hand1_value + dict_value[hand_1[i]]
-        if hand_1[i] == "A":
-            countA_1 = countA_1 + 1
-    hand1_value = testA(hand1_value, countA_1)
+#     for i in range(len(hand_1)):
+#         hand1_value = hand1_value + dict_value[hand_1[i]]
+#         if hand_1[i] == "A":
+#             countA_1 = countA_1 + 1
+#     hand1_value = testA(hand1_value, countA_1)
 
-    for j in range(len(hand_2)):
-        hand2_value = hand2_value + dict_value[hand_2[j]]
-        if hand_2[j] == "A":
-            countA_2 = countA_2 + 1
-    hand2_value = testA(hand2_value, countA_2)
+#     for j in range(len(hand_2)):
+#         hand2_value = hand2_value + dict_value[hand_2[j]]
+#         if hand_2[j] == "A":
+#             countA_2 = countA_2 + 1
+#     hand2_value = testA(hand2_value, countA_2)
 
-    print(hand1_value, '\n',  hand2_value)
-    if ((hand1_value  <= hand2_value) and hand2_value <= 21) or (hand1_value > 21):
-        return False
-    else:
-        return True
+#     print(hand1_value, '\n',  hand2_value)
+#     if ((hand1_value  <= hand2_value) and hand2_value <= 21) or (hand1_value > 21):
+#         return False
+#     else:
+#         return True
 
 
-print(blackjack_hand_greater_than(['10', '7'], ['8', '3', 'A', '5']))        
-# Check your answer
-#q3.check()
+# print(blackjack_hand_greater_than(['10', '7'], ['8', '3', 'A', '5']))        
+# # Check your answer
+# #q3.check()
+
+
+
+#recursion
+# Solution 1
+# f = lambda n:1 if n in (1,2) else f(n-1) + f(n-2)
+
+# Solution 2
+# def fib(n):
+#     a,b = 1,1
+#     for i in range(n-1):
+#         a,b = b, a+b
+#         print(a," ", b, '\n')
+#     return a
+
+# Solution 3
+def catche(f):
+    memo = {}
+    
+    def helper(x):
+        if x not in memo:
+            memo[x] = f(x)
+            print(memo)
+            print(x)
+        return memo[x]
+
+    return helper
+
+
+
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print ('%r  %2.2f ms' % \
+                  (method.__name__, (te - ts) * 1000))
+        return result
+    return timed
+
+
+
+@timeit
+@catche
+def fibR(n):
+    if n == 1 or n == 2:
+        return 1
+    print(n)
+    return fibR(n-1) +fibR(n-2)
+
+fibR(10)
